b06959cddac9ea7c514be20461cae27b7bc3ba6d
Nikunj-daga-94 <dodmst@gmail.com>
2020-11-21 14:53:58 +0530

    4 files modified

--- src/app/table-map/table-map.component.ts
+++ src/app/table-map/table-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { agentMock } from "../products";
 import transcript from "../../assets/transcript.json";
+// import { MatTableDataSource } from "@angular/material/table/table-data-source";
 export interface PeriodicElement {
   time: String;
   sentence: String;
@@ -26,5 +27,17 @@ const Multidata: PeriodicElement[] = transcript.transcript.map(data => {
 export class TableMapComponent {
   constructor() {}
   dataSource = Multidata;
+  dataSource2 = transcript.script.map(data => {
+    return {
+      line: data.order + 1,
+      matching_sentence:
+        data.similarity * 100 + "% match " + data.matching_sentence,
+      sentence: data.sentence
+    };
+  });
   displayedColumns: string[] = ["time", "channel", "sentence"];
+  displayedCol2: string[] = ["sentence", "line"];
+  showDefinition(event) {
+    console.log("Event", event.target);
+  }
 }
